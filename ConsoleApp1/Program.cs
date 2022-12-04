6fb8c00b0e584a459f3291f8a1374d5f4b85be4e
Evgeny Karpov <evka@netcompany.com>
2022-12-04 08:12:06 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿
+string[] input = System.IO.File.ReadAllLines(@"C:\Users\evka\Documents\adventofcode.txt");
+
+static int ConvertToscore(char c)
+{
+    byte test = (byte)c;
+    return Char.IsUpper(c) ? (int)test - 38 : (int)test - 96;
+}
+
+var sum = 0;
+for (int i = 0; i < input.Length; i = i + 3)
+{
+    foreach (char c in input[i])
+    {
+        if (input[i + 1].Contains(c) && input[i + 2].Contains(c))
+        {
+            Console.WriteLine(ConvertToscore(c) + " - " + c);
+            sum += ConvertToscore(c);
+            break;
+        }
+    }
+
+}
+foreach (string line in input)
+{
+    var firstRucksackLength = line.Length / 2;
+    var secondRucksackLength = line.Length - firstRucksackLength;
+    var firstRucksack = line.Substring(0, firstRucksackLength);
+    var secondRucksack = line.Substring(line.Length / 2, secondRucksackLength);
+    foreach (char c in firstRucksack)
+    {
+        if (secondRucksack.Contains(c))
+        {
+            sum += ConvertToscore(c);
+            Console.WriteLine(c);
+            break;
+        }
+    }
+
+    Console.WriteLine("RESULTAT = " + sum);
